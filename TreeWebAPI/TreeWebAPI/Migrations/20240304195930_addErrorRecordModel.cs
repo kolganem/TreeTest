49b351c8155dd8329891657ce00feb3fb8c6afde
Alexei Kolhan <ak@centaurea.io>
2024-03-04 23:12:06 +0300

    add migration for errorRecord

---
+++ TreeWebAPI/TreeWebAPI/Migrations/20240304195930_addErrorRecordModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TreeWebAPI.Migrations
+{
+    public partial class addErrorRecordModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ErrorRecords",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Time = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    TypeInfo = table.Column<string>(type: "text", nullable: false),
+                    Data = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ErrorRecords", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ErrorRecords");
+        }
+    }
+}
