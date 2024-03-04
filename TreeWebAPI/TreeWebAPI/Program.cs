09c1f0db043d7c9726d083019d912f8d7b30b987
Alexei Kolhan <ak@centaurea.io>
2024-03-04 20:12:10 +0300

    init commit

---
+++ TreeWebAPI/TreeWebAPI/Program.cs
@@ -0,0 +1,49 @@
+using System.Text.Json.Serialization;
+using StoreProject.Infrastructure;
+using TreeWebAPI;
+using TreeWebAPI.Infrastructure;
+using TreeWebAPI.Infrastructure.Middleware;
+using TreeWebAPI.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddDbContext<TreeDbContext>();
+builder.Services.AddControllers();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve;
+});
+
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<ITreeNodeRepository, TreeNodeRepository>();
+builder.Services.AddScoped<IErrorRecordRepository, ErrorRecordRepository>();
+
+builder.Services.AddScoped<ITreeNodeService, TreeNodeService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseMiddleware<GlobalExceptionMiddleware>();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Seed();
+
+app.Run();
